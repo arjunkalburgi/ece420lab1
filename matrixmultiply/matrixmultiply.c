913f3f42ad0789e38e42745b08f313eea1a33c12
Arjun Kalburgi <askalburgi@gmail.com>
2018-01-15 18:07:26 -0700

    change thread num var

--- matrixmultiply/matrixmultiply.c
+++ matrixmultiply/matrixmultiply.c
@@ -2,75 +2,75 @@
 
 
 int main (int argc, char * argv[]){
-    return 0; 
-// 	number_threads = parse_number_threads(argc, argv);
-// 	pthread_t threads[number_threads];
 
-// 	Lab1_loadinput(&A, &B, &n);
+	thread_number = get_num_threads(argc, argv);
+	pthread_t threads[thread_number];
 
-// 	create_result_matrix(n);
+	Lab1_loadinput(&A, &B, &n);
 
-// 	double start_time;
-// 	double end_time;
-// 	GET_TIME(start_time);
+	create_result_matrix(n);
 
-// 	int i;
-// 	for (i = 0; i < number_threads; i++){
-// 		pthread_create(&threads[i], NULL, multiplyBlock, (void *) i);
-// 	}
+	double start_time;
+	double end_time;
+	GET_TIME(start_time);
 
-// 	for (i = 0; i < number_threads; i++){
-// 		pthread_join(threads[i], NULL);
-// 	}
+	int i;
+	for (i = 0; i < thread_number; i++){
+		pthread_create(&threads[i], NULL, multiplyBlock, (void *) i);
+	}
 
-// 	GET_TIME(end_time);
+	for (i = 0; i < thread_number; i++){
+		pthread_join(threads[i], NULL);
+	}
 
-// 	Lab1_saveoutput(C, &n, end_time - start_time);
+	GET_TIME(end_time);
 
-// 	return 0;
-// }
+	Lab1_saveoutput(C, &n, end_time - start_time);
 
-// void* multiplyBlock(void* threadID){
-// 	int k = (int) threadID;
-// 	int p = number_threads;
+	return 0;
+}
 
-// 	int x = k / sqrt(p);
-// 	int y = (int) k % (int) sqrt(p);
+void* multiplyBlock(void* threadID){
+	int k = (int) threadID;
+	int p = number_threads;
 
-// 	int row_lower_bound = (n/sqrt(p)) * x;
-// 	int column_lower_bound = (n/sqrt(p)) * y;
+	int x = k / sqrt(p);
+	int y = (int) k % (int) sqrt(p);
 
-// 	int row_upper_bound = ((n/sqrt(p))*(x+1)) -1;
-// 	int column_upper_bound = ((n/sqrt(p))*(y+1)) -1;
+	int row_lower_bound = (n/sqrt(p)) * x;
+	int column_lower_bound = (n/sqrt(p)) * y;
 
-// 	solve_block(row_lower_bound, column_lower_bound, 
-// 	row_upper_bound, column_upper_bound);
+	int row_upper_bound = ((n/sqrt(p))*(x+1)) -1;
+	int column_upper_bound = ((n/sqrt(p))*(y+1)) -1;
+
+	solve_block(row_lower_bound, column_lower_bound, 
+	row_upper_bound, column_upper_bound);
 	
-// 	return 0;
-// }
+	return 0;
+}
 
-// void solve_block(int row_lower_bound, int column_lower_bound, 
-// 	int row_upper_bound, int column_upper_bound){
+void solve_block(int row_lower_bound, int column_lower_bound, 
+	int row_upper_bound, int column_upper_bound){
 
-// 	int i;
-// 	int j;
+	int i;
+	int j;
 
-// 	for (i = row_lower_bound; i <= row_upper_bound ; i++){
-// 		for (j = column_lower_bound; j <= column_upper_bound; j++){
-// 			C[i][j] = multiply_vector(i, j);
-// 		}
-// 	}
-// }
+	for (i = row_lower_bound; i <= row_upper_bound ; i++){
+		for (j = column_lower_bound; j <= column_upper_bound; j++){
+			C[i][j] = multiply_vector(i, j);
+		}
+	}
+}
 
-// int multiply_vector(int i, int j){
-// 	int x;
+int multiply_vector(int i, int j){
+	int x;
 
-// 	int sum = 0;
-// 	for (x = 0; x < n; x++){
-// 		sum += A[i][x] * B[x][j];
-// 	}
+	int sum = 0;
+	for (x = 0; x < n; x++){
+		sum += A[i][x] * B[x][j];
+	}
 
-// 	return sum;
+	return sum;
 }
 
 // void create_result_matrix(int size){
