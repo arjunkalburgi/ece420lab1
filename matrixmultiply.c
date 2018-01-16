d8566c53f6e9501289056a7d5f63972fe7e87a74
Abid Rahman <arahman1@ualberta.ca>
2018-01-15 19:05:18 -0700

    IT COMPILES BOIIIIIII

--- matrixmultiply.c
+++ matrixmultiply.c
@@ -14,78 +14,78 @@ int main (int argc, char * argv[]){
 
  	Lab1_loadinput(&A, &B, &n);
 
-// 	create_result_matrix(n);
+	create_result_matrix(n);
 
-// 	double start_time;
-// 	double end_time;
-// 	GET_TIME(start_time);
+	double start_time;
+	double end_time;
+	GET_TIME(start_time);
 
-// 	int i;
-// 	for (i = 0; i < thread_number; i++){
-// 		pthread_create(&threads[i], NULL, multiplyBlock, (void *) i);
-// 	}
+	int i;
+	for (i = 0; i < thread_number; i++){
+		pthread_create(&threads[i], NULL, multiplyBlock, (void *) i);
+	}
 
-// 	for (i = 0; i < thread_number; i++){
-// 		pthread_join(threads[i], NULL);
-// 	}
+	for (i = 0; i < thread_number; i++){
+		pthread_join(threads[i], NULL);
+	}
 
-// 	GET_TIME(end_time);
+	GET_TIME(end_time);
 
-// 	Lab1_saveoutput(C, &n, end_time - start_time);
+	Lab1_saveoutput(C, &n, end_time - start_time);
 
-// 	return 0;
-// }
+	return 0;
+}
 
-// void* multiplyBlock(void* threadID){
-// 	int k = (int) threadID;
-// 	int p = number_threads;
+void* multiplyBlock(void* threadID) {
+	int k = (int) threadID;
+	int p = thread_number;
 
-// 	int x = k / sqrt(p);
-// 	int y = (int) k % (int) sqrt(p);
+	int x = k / sqrt(p);
+	int y = (int) k % (int) sqrt(p);
 
-// 	int row_lower_bound = (n/sqrt(p)) * x;
-// 	int column_lower_bound = (n/sqrt(p)) * y;
+	int row_lower_bound = (n/sqrt(p)) * x;
+	int column_lower_bound = (n/sqrt(p)) * y;
 
-// 	int row_upper_bound = ((n/sqrt(p))*(x+1)) -1;
-// 	int column_upper_bound = ((n/sqrt(p))*(y+1)) -1;
+	int row_upper_bound = ((n/sqrt(p))*(x+1)) -1;
+	int column_upper_bound = ((n/sqrt(p))*(y+1)) -1;
 
-// 	solve_block(row_lower_bound, column_lower_bound, 
-// 	row_upper_bound, column_upper_bound);
+	solve_block(row_lower_bound, column_lower_bound, 
+	row_upper_bound, column_upper_bound);
 	
 	return 0;
 }
 
-// void solve_block(int row_lower_bound, int column_lower_bound, 
-// 	int row_upper_bound, int column_upper_bound){
+void solve_block(int row_lower_bound, int column_lower_bound, 
+	int row_upper_bound, int column_upper_bound){
 
-// 	int i;
-// 	int j;
+	int i;
+	int j;
 
-// 	for (i = row_lower_bound; i <= row_upper_bound ; i++){
-// 		for (j = column_lower_bound; j <= column_upper_bound; j++){
-// 			C[i][j] = multiply_vector(i, j);
-// 		}
-// 	}
-// }
+	for (i = row_lower_bound; i <= row_upper_bound ; i++){
+		for (j = column_lower_bound; j <= column_upper_bound; j++){
+			C[i][j] = multiply_vector(i, j);
+		}
+	}
+}
 
-// int multiply_vector(int i, int j){
-// 	int x;
+int multiply_vector(int i, int j){
+	int x;
 
-// 	int sum = 0;
-// 	for (x = 0; x < n; x++){
-// 		sum += A[i][x] * B[x][j];
-// 	}
+	int sum = 0;
+	for (x = 0; x < n; x++){
+		sum += A[i][x] * B[x][j];
+	}
 
-// 	return sum;
-// }
+	return sum;
+}
 
-// void create_result_matrix(int size){
-// 	C = malloc(size * sizeof(int *));
-// 	int i;
-// 	for (i = 0; i < size; i++){
-// 		C[i] = malloc (size * sizeof(int *));
-// 	}
-// }
+void create_result_matrix(int size){
+	C = malloc(size * sizeof(int *));
+	int i;
+	for (i = 0; i < size; i++){
+		C[i] = malloc (size * sizeof(int *));
+	}
+}
 
 int get_num_threads(int argc, char** argv){
 
--- matrixmultiply.h
+++ matrixmultiply.h
@@ -6,5 +6,10 @@ int thread_number;
 
 void print_usage();
 int get_num_threads(int argc, char * argv[]);
+void create_result_matrix(int size);
+int multiply_vector(int i, int j);
+void solve_block(int row_lower_bound, int column_lower_bound, 
+	int row_upper_bound, int column_upper_bound);
+void* multiplyBlock(void* threadID);
 
 #endif
